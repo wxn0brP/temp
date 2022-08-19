34c6bf7b7a9bfd2521fb1f40ce676a7dfffac33e
wxn0brP <105448874+wxn0brP@users.noreply.github.com>
2022-08-19 20:40:03 +0200

    Add files via upload

--- js/render.js
+++ js/render.js
@@ -1,20 +1,20 @@
 var Render = {
 	dane: {},
-	
+
 	render: function(dane){
 		dane.canvas.Ctx.setTransform(1,0,0,1,0,0);
 		dane.canvas.Ctx.clearRect(0, 0, dane.canvas.Canvas.width, dane.canvas.Canvas.height);
 		dane.canvas.Ctx.translate(-(dane.gracz.x-dane.canvas.Canvas.width/2), -(dane.gracz.y-dane.canvas.Canvas.height/2));
-		
+
 		dane.teren.forEach(function(t){
 			Render.kwadrat(dane, t.x, t.y, t.w, t.h, t.color);
 		});
-		
+
 		dane.przejscia.forEach(function(t){
 			Render.kwadrat(dane, t.x, t.y, t.w, t.h, t.color);
 			Render.dymek(dane, t.x+10, t.y+t.h/2, "hp:\n"+Math.round(t.hp), "#112A46");
 		});
-		
+
 		dane.moby.forEach(function(t, index){
 			if(dane.config.dev){
 				Render.border(dane, t.x-t.oczy+t.w/2, t.y-t.oczy+t.h/2, t.oczy*2, t.oczy*2);
@@ -23,7 +23,7 @@ var Render = {
 			Render.kwadrat(dane, t.x, t.y, t.w, t.h, t.color);
 			Render.tekst(dane, t.x+t.w/2, t.y-5, "hp: "+Math.round(t.hp));
 		});
-		
+
 		Render.kwadrat(dane, dane.gracz.x, dane.gracz.y, dane.gracz.w, dane.gracz.h, "red");
 		let x = dane.gracz.x;
 		let y = dane.gracz.y;
@@ -41,7 +41,7 @@ var Render = {
 			x += dane.gracz.w/2-2.5;
 		}
 		Render.kwadrat(dane, x, y, 5, 5, "yellow");
-		
+
 		if(dane.config.dev){
 			Render.border(
 				dane,
@@ -51,55 +51,18 @@ var Render = {
 				dane.gracz.oczy*2
 			);
 		}
-		
+
 		dane.canvas.Ctx.setTransform(1,0,0,1,0,0);
-		
+
 		His.dane.pow.forEach(function(t){
 			Render.dymek(dane, t.x, t.y, t.co, "white", 14);
 		});
-	},
-	
-	kwadrat: function(dane, x, y, w, h, color){
-		dane.canvas.Ctx.beginPath();
-		dane.canvas.Ctx.fillStyle = color;
-		dane.canvas.Ctx.fillRect(x, y, w, h);
-		dane.canvas.Ctx.fill();
-		dane.canvas.Ctx.closePath();
-	},
-	
-	border: function(dane, x, y, w, h){
-		dane.canvas.Ctx.beginPath();
-		dane.canvas.Ctx.fillStyle = "rgba(0%, 0%, 0%, 0)";
-		dane.canvas.Ctx.rect(x, y, w, h);
-		dane.canvas.Ctx.fill();
-		dane.canvas.Ctx.stroke();
-		dane.canvas.Ctx.closePath();
-	},
-	
-	tekst: function(dane, x, y, t, color="white", r=10){
-		dane.canvas.Ctx.font = r+"px Arial";
-		dane.canvas.Ctx.fillStyle = color;
-		dane.canvas.Ctx.textAlign = "center";
-		dane.canvas.Ctx.fillText(t, x, y)
-	},
-	
-	linia: function(dane, x, y, x2, y2){
-		dane.canvas.Ctx.beginPath();
-		dane.canvas.Ctx.moveTo(x, y);
-		dane.canvas.Ctx.lineTo(x2, y2)
-		dane.canvas.Ctx.moveTo(0, 0);
-		dane.canvas.Ctx.stroke();
-		dane.canvas.Ctx.closePath();
-	},
-	
-	dymek: function(dane, x, y, co, color, r=10, libr=15){
-		var lines = co.split('\n');
 
-		for(let i=0; i<lines.length; i++){
-			Render.tekst(dane, x, y + (i*libr), lines[i], color, r);
+		if(Akt.dane.zapisano){
+			Render.dymek(dane, 900, 20, "ZAPISANO", "white", 14);
 		}
 	},
-	
+
 	menu: function(dane){
 		Render.render(dane);
 		Akt.info(dane);
@@ -137,14 +100,17 @@ var Render = {
 					Silnik.nowaGra(dane);
 				}else if(d == "zapis"){
 					Silnik.zapis(dane);
+				}else if(d == "auto zapis"){
+					dane.config.autoZapis = !dane.config.autoZapis;
+					alert("auto zapis = "+dane.config.autoZapis);
 				}else if(d == "muzyka"){
 					Silnik.muzyka(dane);
 				}else if(d == "res pos"){
 					dane.gracz.x = 495;
 					dane.gracz.y = 245;
 				}else if(d == "rand pos"){
-					dane.gracz.x = __.rand(-1000, 1200);
-					dane.gracz.y = __.rand(-1000, 1200);
+					dane.gracz.x += __.rand(-300, 300);
+					dane.gracz.y += __.rand(-300, 300);
 				}else if(d == "sklep"){
 					let skl = prompt(
 						"--POSIADAMY--\n"+
@@ -193,6 +159,7 @@ var Render = {
 					"--POLECENIA--\n"+
 					"- sklep \t\t\t --> poprostu sklep\n"+
 					"- zapis \t\t\t --> zapisuje postęp\n"+
+					"- auto zapis \t\t --> co jakisz czas zapisuje postęp\n"+
 					"- muzyka \t\t --> przełącza muzyke w tle\n"+
 					"- res pos \t\t --> resrtuje pozycje\n"+
 					"- rand pos \t\t --> przenosi cię w randomową pozycje\n"+
@@ -200,5 +167,48 @@ var Render = {
 				);
 			}
 		}
-	}
+	},
+
+
+	kwadrat: function(dane, x, y, w, h, color){
+		dane.canvas.Ctx.beginPath();
+		dane.canvas.Ctx.fillStyle = color;
+		dane.canvas.Ctx.fillRect(x, y, w, h);
+		dane.canvas.Ctx.fill();
+		dane.canvas.Ctx.closePath();
+	},
+
+	border: function(dane, x, y, w, h){
+		dane.canvas.Ctx.beginPath();
+		dane.canvas.Ctx.fillStyle = "rgba(0%, 0%, 0%, 0)";
+		dane.canvas.Ctx.rect(x, y, w, h);
+		dane.canvas.Ctx.fill();
+		dane.canvas.Ctx.stroke();
+		dane.canvas.Ctx.closePath();
+	},
+
+	tekst: function(dane, x, y, t, color="white", r=10){
+		dane.canvas.Ctx.font = r+"px Arial";
+		dane.canvas.Ctx.fillStyle = color;
+		dane.canvas.Ctx.textAlign = "center";
+		dane.canvas.Ctx.fillText(t, x, y)
+	},
+
+	linia: function(dane, x, y, x2, y2){
+		dane.canvas.Ctx.beginPath();
+		dane.canvas.Ctx.moveTo(x, y);
+		dane.canvas.Ctx.lineTo(x2, y2)
+		dane.canvas.Ctx.moveTo(0, 0);
+		dane.canvas.Ctx.stroke();
+		dane.canvas.Ctx.closePath();
+	},
+
+	dymek: function(dane, x, y, co, color, r=10, libr=15){
+		var lines = co.split('\n');
+
+		for(let i=0; i<lines.length; i++){
+			Render.tekst(dane, x, y + (i*libr), lines[i], color, r);
+		}
+	},
+
 };
